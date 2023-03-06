425ed223aaf15dab6c7a3dc393ca3ba2b9f12843
fardinnafis <70797988+fardinnafis@users.noreply.github.com>
2023-03-07 00:41:42 +0600

    Add files via upload

---
+++ cargame.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+running = True
+screen=pygame.display.set_mode((800,800))
+while running :
+    for event in pygame.event.get():
+        if event.type == QUIT :
+            running = False
+
+
+pygame.quit()
